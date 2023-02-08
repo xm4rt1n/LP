c8ed83d479c4be5f889064a5068824b9078a5fe6
xm4rt1n <adrianmartin2505@gmail.com>
2023-02-08 20:16:37 +0100

    Hoja ejercicios 0

--- EjercicioTuplas/src/Ejercicio.hs
+++ EjercicioTuplas/src/Ejercicio.hs
@@ -1,11 +1,28 @@
 module Ejercicio where
 
-    hayInterseccion :: (Int, Int) -> (Int, Int) -> Bool
-    hayInterseccion (x,y) (m,n) = x == y
+    -- funcion que nos da el mayor entre el resto y el cociente entre x y n
+    componer :: (Int, Int) -> Int
+    componer (x, n) 
+        | (x `mod` n) > (x `div` n) = x `mod` n
+        | otherwise = x `div` n
 
+    -- funcion que nos da el sucesor al nunero introducido
+    sucesor :: Int -> Int
+    sucesor x = x+1    
+
+    -- funcion que nos devuelve el doble de un numero dado
+    doble :: Int -> Int
+    doble x = 2*x
+
+    -- funcion que nos devuelve el cuadruple de un numero dado llamando dos veces a la funcion doble
+    cuadruple :: Int -> Int
+    cuadruple x = doble(doble x)
+
+    -- funcion que nos devuele el mayor de los numeros introducidos
     mayor :: (Int, Int) -> Int
     mayor (x, y)
         | x > y = x
         | otherwise = y
 
   
+    
