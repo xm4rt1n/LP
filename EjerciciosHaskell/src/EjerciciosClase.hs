65e2fbbc66f4f722de0d9c3d6a493127952da509
Adrian <adrianmartin2505@gmail.com>
2023-03-09 23:10:21 +0100

    ejerciciosClase T5

--- EjerciciosHaskell/src/EjerciciosClase.hs
+++ EjerciciosHaskell/src/EjerciciosClase.hs
@@ -90,3 +90,44 @@ module EjerciciosClase where
     listaFunciones'' lista n = foldr (\f ac -> [f n] ++ ac) [] lista
 
     
+    --------------------------------------------------- Tema 5 ---------------------------------------------------
+    data Arbol a = AV | Rama (Arbol a) a (Arbol a) deriving (Show, Eq)
+
+    a1 = Rama (Rama (Rama AV 12 AV) 49 (Rama (Rama AV 23 AV) 5 (Rama AV 13 AV))) 123 (Rama AV 10 AV)
+    a2 = Rama (Rama (Rama AV 12 AV) 49 (Rama (Rama AV 23 AV) 5 (Rama AV 13 AV))) 123 (Rama AV 10 AV)
+
+    hojas :: Arbol a -> [a]
+    hojas AV = []
+    hojas (Rama AV h AV) = [h]
+    hojas (Rama izq r der) = hojas izq ++ hojas der
+    -- posicion :: [Int] -> Int -> Maybe Int
+    -- posicion lista num = posicionAux lista num 0
+    
+    -- Dada la siguiente declaracion de arbol polimorfico, se pide implementar una funcion que diga si dos arboles diga si son iguales
+   
+
+    arbolesIguales :: (Eq a) => Arbol[a] -> Arbol[a] -> Bool
+    arbolesIguales AV AV = True
+    arbolesIguales AV _ = False
+    arbolesIguales _ AV = False
+    arbolesIguales (Rama i1 r1 d1) (Rama i2 r2 d2) = r1 == r2 && arbolesIguales i1 i2 && arbolesIguales d1 d2
+
+    -- si se hace creando una instancia de la clase Eq para el tipo arbol, se estaria implementando la funcion == para comparar arbolesIguales
+    --instance (Eq a) => Eq (Arbol a) where
+    --    AV == AV == True
+    --    AV == _ = False
+    --    _ == AV = False
+    --    (Rama i1 r1 d1) == (Rama i2 r2 d2) 
+
+
+    -- se pide una funcion polimorfica que dada una lista de elem devuelva el menor
+    menorPolimorfica :: Ord a => [a] -> Maybe a
+    menorPolimorfica [] = Nothing
+    menorPolimorfica[x] = Just x
+    menorPolimorfica (x:y:xs) =  menorPolimorfica ((min x y):xs)
+
+    -- se pide una funcion que dado un elemento y una lista de tuplas de dos elementos, busque la tupla donde el elemtno
+    -- dado esta en primer lugar y devuelva su pareja en la tupla.
+    --buscarPareja :: Eq a => a -> [(a, b)] -> Maybe b
+    --buscarPareja x [] = Nothing
+    --buscarPareja x [(m, n):xs] = 
